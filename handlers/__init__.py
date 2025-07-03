42b5ccba209b9ef9b93b2ddbcd8d4467a79adac7
houssin11 <alkal36383@gmail.com>
2025-07-03 20:37:41 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -11,3 +11,20 @@ from . import admin_panel
 from . import admin_broadcast
 from . import callback_handler
 from . import buy
+
+# تعريف واضح للوظائف التي سيتم تصديرها
+__all__ = [
+    'start',
+    'welcome',
+    'products',
+    'payment',
+    'wallet',
+    'transfer',
+    'history',
+    'support',
+    'admin_report',
+    'admin_panel',
+    'admin_broadcast',
+    'callback_handler',
+    'buy'
+]
